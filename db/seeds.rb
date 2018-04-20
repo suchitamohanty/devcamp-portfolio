b164199a7e4fa691bdfb2d13a98414ee05bd8740
Suchita Mohanty <mohanty.suchita@gmail.com>
2018-04-20 17:34:30 +0530

    Intigrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,16 +31,16 @@ puts "5 skills created"
 		title: "Portfolio title #{portfolio_item}",
 		subtitle: "My great service",
 		body: "It is a long established fact that a reader will be
-		distracted by the readable content of a page when looking
-		at its layout. The point of using Lorem Ipsum is that it
-		has a more-or-less normal distribution of letters, as opposed
-		to using 'Content here, content here', making it look like
-		readable English. Many desktop publishing packages and web page
-		editors now use Lorem Ipsum as their default model text, and
-		a search for 'lorem ipsum' will uncover many web sites still in
-		their infancy.",
-		maain_image: "http://via.placeholder.com/600x400",
-		maain_image: "http://via.placeholder.com/350x200"
+			distracted by the readable content of a page when looking
+			at its layout. The point of using Lorem Ipsum is that it
+			has a more-or-less normal distribution of letters, as opposed
+			to using 'Content here, content here', making it look like
+			readable English. Many desktop publishing packages and web page
+			editors now use Lorem Ipsum as their default model text, and
+			a search for 'lorem ipsum' will uncover many web sites still in
+			their infancy.",
+		maain_image: "http://via.placeholder.com/350x150",
+		thumb_image: "http://via.placeholder.com/350x200"
 	)
 end
 
